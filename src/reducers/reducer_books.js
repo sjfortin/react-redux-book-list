e308d6e113ff724aa8638ce23f9eef8ad1496044
Sam Fortin <sam.j.fortin@gmail.com>
2017-11-13 22:41:37 -0600

    Init commit. BookList reducer added. Container class introduced

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Harry Potter 1' },
+    { title: 'Harry Potter 2' },
+    { title: 'Harry Potter 3' },
+    { title: 'Where the Wild Things Are' }
+  ];
+}
