189518c0fd2da446cb1d92c30931bf8715fdce95
Sam Fortin <sam.j.fortin@gmail.com>
2017-11-22 15:36:55 -0600

    Active book actions and redcuers implemented

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Harry Potter 1' },
-    { title: 'Harry Potter 2' },
-    { title: 'Harry Potter 3' },
-    { title: 'Where the Wild Things Are' }
+    { title: 'Harry Potter 1', pages: 103 },
+    { title: 'Harry Potter 2', pages: 45 },
+    { title: 'Harry Potter 3', pages: 65 },
+    { title: 'Where the Wild Things Are', pages: 89 }
   ];
 }
